e75747e4eac4d1ace1f7c2618e7754c2736a4ad7
Lucas Henrique Torresin da Costa <lucas_hcosta@live.com>
2023-07-07 10:09:17 -0300

    fixing bugs

--- src/components/ProfileImage.tsx
+++ src/components/ProfileImage.tsx
@@ -2,15 +2,15 @@ import { type StaticImport } from "next/dist/shared/lib/get-img-props"
 import Image from "next/image"
 
 type ProfileImageProps = {
-  src: string  | StaticImport,
+  src: string  | StaticImport | undefined | null,
   className?: string,
-}
+} 
 
 
 export default function ProfileImage({src, className = ""}: ProfileImageProps) {
   return (
     <div className={`relative h-12 w-12 overflow-hidden rounded-full ${className}`}>
-        {src === null ? null : <Image src={src} alt="Profile Image" quality={100} fill/>}
+        {src == null ? null : <Image src={src} alt="Profile Image" quality={100} fill/>}
     </div>
   )
 }
